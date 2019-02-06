0ca9fccd57b768d76f7d890118e317956d7c1e0f
Eve <eve.chen@rocketmail.com>
2019-02-06 10:58:48 -0700

    Assignment Cloned

---
+++ Assignment1/Assets/Scripts/ModelMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelMovement : MonoBehaviour
+{
+    public Animator anim;
+    private Vector3 movementVector;
+    public float speed = 100f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        movementVector = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit hit;
+            Physics.Raycast(ray, out hit);
+            try
+            {
+                if (hit.collider.tag == "Ground")
+                {
+                    Vector3 v = hit.point;
+                    movementVector = new Vector3(v.x, transform.position.y, v.z);
+                    transform.LookAt(movementVector);
+                }
+            }
+            catch
+            {
+            }
+            iTween.MoveTo(gameObject, movementVector, 20f);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.GetComponent<Animation>().Play("0008_Win"); 
+    }
+}
